86a9603feef283ced44a82949f261cde06c266ab
Sami Daniel <samidanielpersonal@gmail.com>
2025-03-06 12:10:00 -0300

    Change file location

---
+++ ChatApplication.Consumer/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ChatApplication.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ChatApplication.Consumer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        //{
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=localhost;database=192.168.2.100:3007;uid=root;pwd=98sf7d4pj;port=4003;";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<Message> Messages { get; set; } = null!;
+    }
+}
