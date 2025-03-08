74bbf64d539f72559e7d5b2c81e75144d5dac00b
Sami Daniel <samidanielpersonal@gmail.com>
2025-03-08 18:49:30 -0300

    alter to /host the creation of script

--- configdatabase.sh
+++ configdatabase.sh
@@ -1,4 +1,4 @@
-echo 'ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '98sf7d4pj';' >> init.sql
-echo 'FLUSH PRIVILEGES' >> init.sql;
+echo "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';" >> /host/init.sql
+echo "FLUSH PRIVILEGES" >> /host/init.sql;
 
-echo 'Generated init.sql file';
+echo "Generated init.sql file";
